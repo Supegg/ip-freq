1effba8b7da27cc27c3fdae4512525d6d3d610d5
Supegg <supegg.rao@gmail.com>
2017-12-27 17:29:35 +0800

    fix last ip's last-time=0 bug
    format output string
    eg：
    	莫名对齐问题和notepad++和vs2015这两个ide的相关，将tab处理为2个空格

--- AnalyseIp.py
+++ AnalyseIp.py
@@ -5,6 +5,11 @@ import datetime
 """
 analyse ip change history
 
+date:2017-12-27
+note:  
+    fix last ip's last-time=0 bug
+    format output string  
+
 date:2017-12-22
 note:creat&analyse first
 
@@ -33,28 +38,32 @@ with open('loongson_ip_20171221.txt', 'r') as ips:
             if cur[1] != history[-1][1]:
                 history.append(cur)
                 dt = cur[0] - history[-2][0]
-                dt = dt.days*24 + dt.seconds / 3600
+                dt = round(dt.days*24 + dt.seconds / 3600, 2)
                 if dt<min:
                     min=dt
                 if dt>max:
                     max=dt
                 history[-2].append(dt)
-                print(history[-2])
-
-    history[-1].append(0)
-    print(history[-1])
+                print('%s  %s\t%s'%(history[-2][0].strftime('%Y-%m-%d %H:%M:%S'),history[-2][1].ljust(15),history[-2][2]))
+    
+    #calc last ip's last-time
+    dt = datetime.datetime.now()-history[-2][0]
+    dt = dt.days*24 + round(dt.seconds / 3600, 2)
+    history[-1].append(dt)
+    print('%s  %s\t%s'%(history[-1][0].strftime('%Y-%m-%d %H:%M:%S'),history[-1][1].ljust(15),history[-1][2]))
 
     #calc run time
-    print("begin: %s"%d1.strftime("%H:%M:%S"))
-    print("end:   %s"%datetime.datetime.now().strftime("%H:%M:%S"))
     dt = datetime.datetime.now() - d1
     dt = dt.seconds + dt.microseconds / 1000000
 
-print("run time: %s s" %dt)
-print("total num: %d" %(cnt))
-print("total time: %d days" %((history[-1][0]-history[0][0]).days))
-print("change num: %s" %len(history))
-print("min: %d" %min)
-print("max: %d" %max)
+print('\nsummary')
+print("\tbegin:\t\t %s"%d1.strftime("%H:%M:%S"))
+print("\tend:\t\t %s"%datetime.datetime.now().strftime("%H:%M:%S"))
+print("\trun time:\t %.1f s" %dt)
+print("\trecord num:\t %d" %(cnt))
+print("\trecord time:\t %d days" %((history[-1][0]-history[0][0]).days))
+print("\trecord ips:\t %d times" %len(history))
+print("\tmin:\t\t %d hour" %min)
+print("\tmax:\t\t %d hours" %max)
 
 #print format: http://www.runoob.com/w3cnote/python3-print-func-b.html
